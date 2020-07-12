1cf5f6ca909b38b5eab53037cf6f8b805ecb4cf2
Jesus Castellanos <jcastellanos1007@gmail.com>
2020-07-12 18:00:12 -0400

    Change URL with certificate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://ec2-3-136-25-68.us-east-2.compute.amazonaws.com:8082/'
+  url: 'https://ec2-3-136-25-68.us-east-2.compute.amazonaws.com:8442/'
 };
